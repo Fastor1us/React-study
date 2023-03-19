2b3dd929b47f264ace09b57e5101fac5cb1808bf
Кирюшин Олег <okiryushin@data-platform.ru>
2023-03-19 22:23:16 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Counter from './components/Counter'
+import ClassCounter from './components/ClassCounter'
+import './styles/App.css'
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Counter/>
+      <ClassCounter/>
+
+      <div className="post">
+        <div className="post__content">
+          <strong>1. Основы React</strong>
+          <div>
+            React - веб-фреймворк, свободное и открытое ПО, библиотека JS
+          </div>
+        </div>
+        <div className="post__btns">
+          <button>Удалить</button>
+        </div>
+      </div>
+
     </div>
   );
+
 }
 
 export default App;
